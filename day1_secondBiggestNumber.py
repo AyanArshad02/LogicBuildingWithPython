f9fb4d4647b8911093dc92e200c0fb5098a7afa6
AyanArshad02 <ayanarshad2002@gmail.com>
2025-06-26 18:38:33 +0530

    add 2

--- day1_secondBiggestNumber.py
+++ day1_secondBiggestNumber.py
@@ -1,17 +1,15 @@
 def find_second_largest(lst):
     if len(lst) < 2:
-        return -1  # Not enough elements
+        return -1
 
     max_val = float('-inf')
 
-    # First pass – Find the maximum value
     for num in lst:
         if num > max_val:
             max_val = num
 
     second_max = -1
 
-    # Second pass – Find the largest value less than max_val
     for num in lst:
         if num != max_val and num > second_max:
             second_max = num
@@ -19,6 +17,6 @@ def find_second_largest(lst):
     return second_max
 
 
-print(find_second_largest([12, 35, 1, 10, 34, 1]))   # Output: 34
-print(find_second_largest([10, 5, 10]))              # Output: 5
-print(find_second_largest([10, 10, 10]))             # Output: -1
+print(find_second_largest([12, 35, 1, 10, 34, 1]))
+print(find_second_largest([10, 5, 10]))
+print(find_second_largest([10, 10, 10]))
